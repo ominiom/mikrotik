181bff3772a3c0f8775949e6805721794192c8e5
Iain Wilson <iain.iw.wilson@gmail.com>
2012-01-24 17:10:23 +0000

    Fixes

--- doc/setup.rb
+++ doc/setup.rb
@@ -9,12 +9,10 @@ class HasEventsHandler < YARD::Handlers::Ruby::Base
     }.each { |event_name|
       event_name = event_name.jump(:ident).source
       object = YARD::CodeObjects::MethodObject.new(namespace, "on_#{event_name}")
-      # object.docstring = statement.comments || ''
       register(object) 
       object.dynamic = true
       object.docstring.add_tag(YARD::Tags::Tag.new(:return, 'self', namespace.path))
       object.docstring.add_tag(YARD::Tags::Tag.new(:group, 'Events'))      
-      puts object.tags.inspect
     }    
   end
 end
