48cc71444ac75154648d0e5623c8968958701246
Iain Wilson <iain.iw.wilson@gmail.com>
2012-01-28 18:05:37 +0000

    Parser rewrite

--- lib/mikrotik/protocol/parser.rb
+++ lib/mikrotik/protocol/parser.rb
@@ -7,91 +7,120 @@ class Mikrotik::Protocol::Parser
   has_event :sentence
 
   def initialize
-    @data = ""
-    @data.force_encoding(Encoding::BINARY) if RUBY_VERSION >= '1.9.0'
+    @data = ''
+    @data.force_encoding(Encoding::BINARY) if RUBY_VERSION > '1.9'
+    reset!
+  end
+
+  # Discards the sentence currently being parsed
+  def reset!
     @sentence = Mikrotik::Protocol::Sentence.new
   end
 
   # Adds data to the parser buffer and runs the parser
   def <<(data)
     @data << data
-    parse
+    success = true
+    success = get_sentence while success
   end
 
-  private
+  PAIR = /^(.+)=(.+)$/
 
-  def parse
-    Mikrotik.debug [:parser, :running]
-    Mikrotik.debug [:parser, :buffer, @data.size, @data]
-    success, word = get_word
-    return unless success
-    Mikrotik.debug [:parser, :word, word]
-    if word.empty? then
-      Mikrotik.debug [:parser, :on_sentence, @sentence]
-      on_sentence(@sentence)
-      @sentence = Mikrotik::Protocol::Sentence.new
-    else
-      m = /^(.+)=(.+)$/.match(word)
-      unless m.nil?
-        @sentence[m[1]] = m[2]
-      else
-        @sentence[word] = nil
-      end
+  # Indentifies the size of the length field in bits
+  # @return boolean, integer
+  def get_length_size
+    return false if @data.empty?
+
+    # High bits used for length type
+    # Low bits used as first bits of length
+    
+    # 0xxxxxxx = 7  bit length
+    # 10xxxxxx = 14 bit length
+    # 110xxxxx = 21 bit length
+    # 1110xxxx = 28 bit length
+    # 11110000 = 32 bit length follows
+
+    t = @data.unpack('C').first
+
+    return  7 if t &  0b10000000 == 0
+    return 14 if t &  0b01000000 == 0
+    return 21 if t &  0b00100000 == 0
+    return 28 if t &  0b00010000 == 0
+    return 32 if t == 0b11110000
+
+    raise ArgumentError, "Invalid length type encoding"
+  end
+
+  def get_length
+    size = get_length_size
+
+    return false unless size
+
+    need = bytes(size + 1)
+    return false unless @data.size >= need
+
+    length = nil
+    field = @data[0, need].unpack('C*')
+
+    case size
+      when 7
+        length = field[0] & 0x7f
+      when 14
+        length = ((field[0] & 0x3f) <<  8) | field[1]
+      when 21
+        length = ((field[0] & 0x1f) << 16) | field[1] <<  8 | field[2]
+      when 28
+        length = ((field[0] & 0x0f) << 24) | field[1] << 16 | field[2] << 8 | field[3]
+      when 32
+        length = field[1..4].pack('C4').unpack('N').first
     end
-    parse if @data.size > 0
+
+    if length
+      return length
+    else
+      raise ArgumentError, "Invalid word length"
+    end
   end
 
   def get_word
- 
-     unless @data.length > 0
-       return false, nil   ## Not enough data to parse
-     end
- 
-     ## The first byte tells us how the word length is encoded:
-     len = 0
-     len_byte = cbyte(@data, 0)
-     if len_byte & 0x80 == 0
-       len = len_byte & 0x7f
-       if len == 0 then
-         @data[0, 1] = ''
-         return true, ''
-       end
-       i = 1
-     elsif len_byte & 0x40 == 0
-       unless @data.length > 0x81
-         return false, nil   ## Not enough data to parse
-       end
-       len = ((len_byte & 0x3f) << 8) | cbyte(@data, 1)
-       i = 2
-     elsif len_byte & 0x20 == 0
-       unless @data.length > 0x4002
-         return false, nil   ## Not enough data to parse
-       end
-       len = ((len_byte & 0x1f) << 16) | (cbyte(@data, 1) << 8) | cbyte(@data, 2)
-       i = 3
-     elsif len_byte & 0x10 == 0
-       unless @data.length > 0x200003
-         return false, nil   ## Not enough data to parse
-       end
-       len = ((len_byte & 0x0f) << 24) | (cbyte(@data, 1) << 16) | (cbyte(@data, 2) << 8) | cbyte(@data, 3)
-       i = 4
-     elsif len_byte == 0xf0
-       len = (cbyte(@data, 1) << 24) | (cbyte(@data, 2) << 16) | (cbyte(@data, 3) << 8) | cbyte(@data, 4)
-       i = 5
-     else
-       ## This will also catch reserved control words where the first byte is >= 0xf8
-       raise ArgumentError, "String length encoding is invalid"
-     end
-     if @data.length - i < len
-       return false, nil   ## Not enough data to parse
-     end
-     success, data = true, @data[i, len]
-     @data[0, i + len] = ""
-     return success, data
-   end
+    return false if @data.empty?
+    length_size = bytes(get_length_size + 1)
+    length      = get_length
+    
+    total_size  = length_size + length
+    
+    if length && @data.size >= total_size
+      @data.slice!(0, length_size)
+      word = @data.slice!(0, length)
+      Mikrotik.debug [:parser, :got_word, word]
+      return word
+    end
 
-   def cbyte(str, offset)
-     return str[offset].ord
-   end
+    return false
+  end
+
+  def get_sentence
+    while word = get_word 
+      if word.empty?
+        Mikrotik.debug [:parser, :got_sentence, @sentence]
+        on_sentence(@sentence)
+        reset!
+        return true
+      else
+        if word =~ PAIR
+          key, value = word.scan(PAIR).flatten
+          @sentence[key] = value
+        else
+          @sentence[word] = nil
+        end
+      end
+    end
+
+    return false
+  end
+
+  def bytes(bits)
+    (bits / 8.0).ceil
+  end
 
 end
